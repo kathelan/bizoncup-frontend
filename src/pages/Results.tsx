06e04a379f43150d9ecda162a893fb4b879d0697
kathelan <mateuszdeptulski@gmail.com>
2023-04-19 23:25:01 +0200

    added results page retries

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,3 +1,4 @@
+import "./ResultsStyles.css";
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Container} from 'react-bootstrap';
@@ -10,11 +11,42 @@ const ResultsPage = () => {
     const [selectedCategory, setSelectedCategory] = useState<string>('');
     const {t} = useTranslation();
     const apiBaseUrl = import.meta.env.VITE_APP_API_BASE_URL;
+    const maxRetries = 3;
+
+    const fetchWithRetry = async (url: string, options = {}, retries = 0): Promise<Response> => {
+        try {
+            const response = await fetch(url, options);
+            if (response.status === 404) {
+                if (retries < maxRetries) {
+                    const delay = 2 ** retries * 1000;
+                    await new Promise(resolve => setTimeout(resolve, delay));
+                    return fetchWithRetry(url, options, retries + 1);
+                } else {
+                    throw new Error('Max retries reached');
+                }
+            } else {
+                return response;
+            }
+        } catch (error) {
+            if (retries < maxRetries) {
+                const delay = 2 ** retries * 1000;
+                await new Promise(resolve => setTimeout(resolve, delay));
+                return fetchWithRetry(url, options, retries + 1);
+            } else {
+                throw error;
+            }
+        }
+    };
 
     useEffect(() => {
         const fetchResults = async () => {
-            const response = await axios.get(`${apiBaseUrl}/api/ardf/results`);
-            setResults(response.data.COMPETITORS);
+            try {
+                const response = await fetchWithRetry(`${apiBaseUrl}/api/ardf/results`);
+                const data = await response.json();
+                setResults(data.COMPETITORS);
+            } catch (error) {
+                // handle error here
+            }
         };
         fetchResults();
     }, []);
